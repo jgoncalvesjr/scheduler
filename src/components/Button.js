75d26f16a972d40fd0b7bc50790b40bb5e681de2
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-08-31 15:03:19 -0400

    Render and test Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,30 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classNames from "classnames";
+
 export default function Button(props) {
-   return <></>;
-}
+
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+    });
+ 
+   // if (props.confirm) {
+   //   buttonClass += " button--confirm";
+   // }
+
+   // if (props.danger) {
+   //    buttonClass += " button--danger";
+   // }
+ 
+   return (
+      <button
+        className={buttonClass}
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+        {props.children}
+      </button>
+    );
+ }
