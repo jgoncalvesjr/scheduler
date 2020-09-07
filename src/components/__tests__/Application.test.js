1a44a29bb7fd76a931d222b777025de1114db6e0
jgoncalvesjr <jgoncalvesjr@gmail.com>
2020-09-07 18:34:00 -0400

    Implment and pass edit interview and spots left not updated test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -94,4 +94,39 @@ describe("Application", () => {
   
   });
 
+  it('loads data, edits an interview and keeps the spots remaining for Monday the same', async () => {
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    // 3. Click the "Edit" button on the booked appointment.
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(queryByAltText(appointment, "Edit"));
+
+    // 4. "Save" button should be visible in EDIT mode.
+    expect(getByText(appointment, "Save")).toBeInTheDocument();
+
+    // 5. Interviewer and student should be properly changed.
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Joao Goncalves Jr" },
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // 6. Check that the element with the text "Saving" is displayed.
+    expect(getByText(appointment, "Saving")).toBeInTheDocument();
+
+    // 7. New student name should be visible in the saved component.
+    await waitForElement(() => getByText(appointment, "Joao Goncalves Jr"));
+
+    // 8. Available spots left should not be changed.
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+    expect(getByText(day, "1 spot remaining"));
+  });
+
 });
